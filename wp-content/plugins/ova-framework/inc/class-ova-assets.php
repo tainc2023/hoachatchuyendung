fe0a857c80fa4432b867f0f9f06835bb5a09fff1
tainguyencong75 <tainguyencong75@gmail.com>
2023-10-22 14:39:35 +0700

    translate

--- wp-content/plugins/ova-framework/inc/class-ova-assets.php
+++ wp-content/plugins/ova-framework/inc/class-ova-assets.php
@@ -3,17 +3,20 @@ if ( ! defined('ABSPATH') ) {
 	exit();
 }
 
-class Ova_Framework_Assets {
-	public function __construct(){
+if ( ! class_exists('Ova_Framework_Assets') ) {
+	class Ova_Framework_Assets {
+		public function __construct(){
 		// add_action('admin_enqueue_scripts',array($this,'ova_enqueue_admin_scripts'));
-		add_action('wp_enqueue_scripts', array( $this, 'ova_enqueue_scripts' ) );
-	}
+			add_action('wp_enqueue_scripts', array( $this, 'ova_enqueue_scripts' ) );
+		}
 
-	public function ova_enqueue_admin_scripts(){
+		public function ova_enqueue_admin_scripts(){
+		}
+		public function ova_enqueue_scripts(){
+			wp_enqueue_style( 'fontawesome', OFW_URL . 'assets/libs/fontawesome/fontawesome-all.min.css' , array(), '6.4.2', 'all' );
+			wp_enqueue_style( 'ofw-style', OFW_URL . 'assets/css/frontend/style.css' );
+		}
 	}
-	public function ova_enqueue_scripts(){
-		wp_enqueue_style( 'fontawesome', OFW_URL . 'assets/libs/fontawesome/fontawesome-all.min.css' , array(), '6.4.2', 'all' );
-		wp_enqueue_style( 'ofw-style', OFW_URL . 'assets/css/frontend/style.css' );
-	}
+	new Ova_Framework_Assets();
 }
-new Ova_Framework_Assets();
+
---
+++ wp-content/plugins/ova-framework/inc/class-ova-filter.php
@@ -0,0 +1,13 @@
+<?php
+if ( ! defined('ABSPATH') ) {
+	exit();
+}
+
+if ( ! class_exists('Ova_Filter') ) {
+	class Ova_Filter {
+		public function __construct(){
+
+		}
+	}
+	new Ova_Filter();
+}
